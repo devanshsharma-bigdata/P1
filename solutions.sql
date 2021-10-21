5933b5c6a28f6e93879238731225b1caf978c1c0
devanshsharma-bigdata <86596766+devanshsharma-bigdata@users.noreply.github.com>
2021-10-21 12:52:04 +0530

    Update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -1,57 +1,133 @@
-CREATE DATABASE P1;
-USE P1;
+-- Creating a database for the project
+CREATE DATABASE project1
 
-#1
-CREATE TABLE pageviews (
-lang STRING,
-page STRING,
-views INT)
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ' ';
-CREATE TABLE total_pageviews
-AS SELECT page, SUM(views) OVER (PARTITION BY page ORDER BY page)
-AS total_views FROM pageviews
+-- Selecting the database
+use project1;
+
+-- Create a table to upload the data from the files
+CREATE EXTERNAL TABLE IF NOT EXISTS pageviews1(lang STRING,	
+					   page STRING,
+					   views INT)
+					   ROW FORMAT DELIMITED
+					   FIELDS TERMINATED BY ' '
+					   STORED AS TEXTFILE;
+					   
+-- Loading the files into the table
+LOAD DATA INPATH '/user/maria_dev/data/formated_00.txt' INTO TABLE pageviews1;
+LOAD DATA INPATH '/user/maria_dev/data/formated_01.txt' INTO TABLE pageviews1;
+LOAD DATA INPATH '/user/maria_dev/data/formated_02.txt' INTO TABLE pageviews1;
+.
+.
+.
+LOAD DATA INPATH '/user/maria_dev/data/formated_23.txt' INTO TABLE pageviews1;
+
+-- Display the contents of the table 
+select *from pageviews1
+
+-- Counting the total views from each page and storing it in a table
+CREATE TABLE IF NOT EXISTS total_en_pageviews
+AS SELECT DISTINCT(page), SUM(views) OVER (PARTITION BY page ORDER BY page) 
+AS total_views FROM pageviews1 
 WHERE page != 'Main_Page' AND page != 'Special:Search' AND page != '-';
-SELECT DISTINCT page, total_views FROM total_pageviews ORDER BY total_views DESC;
 
-#2
-CREATE TABLE june_clickstream (
-prev STRING,
-curr STRING,
-type STRING,
-n INT)
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY '\t';
-CREATE TABLE total_internal_links
-AS SELECT prev, type, SUM(n) OVER (PARTITION BY prev ORDER BY prev)
-AS ttlClicks_OnIntLinks FROM june_clickstream
-WHERE prev != 'Main_Page' AND type = 'link';
-CREATE TABLE total_internal_links_final
-AS SELECT DISTINCT prev, type, ttlclicks_onintlinks FROM total_internal_links;
-SELECT * FROM total_internal_links_final ORDER BY ttlclicks_onintlinks DESC;
+-- Question 1 - Page with the most amount of views
+SELECT * FROM total_en_pageviews
+WHERE total_views > 100000
+ORDER BY total_views DESC
+LIMIT 1;
 
-#3
-SELECT * FROM total_internal_links_final WHERE prev='Hotel_California' ORDER BY ttlclicks_onintlinks DESC;
 
-#4
-CREATE TABLE all_lang_pageviews (
-	lang STRING,
-	page STRING,
-	views INT)
+-- Create an external table to load the data from the file
+CREATE EXTERNAL TABLE IF NOT EXISTS clickstream (
+	prev STRING,
+	curr STRING,
+	type STRING,
+	occ INT)
 	ROW FORMAT DELIMITED
-	FIELDS TERMINATED BY ' ';
+	FIELDS TERMINATED BY '\t'
+	STORED AS TEXTFILE;
 
-LOAD DATA INPATH '/user/root/all_lang_wikipedia_20th_jan/' INTO TABLE all_lang_pageviews;
+-- Load the data into the table from the file
+LOAD DATA INPATH '/user/maria_dev/data/clickstream-enwiki-2020-12.tsv' INTO TABLE clickstream;
 
-create table german_pageviews as select * from all_lang_pageviews where lang regexp "de";
-select en.page, sum(en.views) as american_views, de.page ,sum(de.views) as german_views
-from pageviews en
-join german_pageviews de
-where en.page = de.page 
-group by en.page, de.page
-having american_views > german_views 
-order by american_views desc, german_views
+-- Question 2 - Page with the most views in which internal links were used
+select prev, sum(occ) as visits from clickstream
+where type = "link" 
+group by prev
+order by visits desc
+limit 10;
+
+-- Create an external table to load the data from the file
+CREATE EXTERNAL TABLE IF NOT EXISTS clickstream (
+	prev STRING,
+	curr STRING,
+	type STRING,
+	occ INT)
+	ROW FORMAT DELIMITED
+	FIELDS TERMINATED BY '\t'
+	STORED AS TEXTFILE;
+
+-- Load the data into the table from the file
+LOAD DATA INPATH '/user/maria_dev/data/clickstream-enwiki-2020-12.tsv' INTO TABLE clickstream;
+
+
+-- Question 3 - Pages starting with "Hotel California" who has the highest views and have clicked internal links
+
+select prev, sum(occ) as visits from clickstream where prev like "%Hotel_California%" group by prev ORDER BY visits DESC limit 20;
+
+(or) 
+
+select prev, sum(pair) as visit 
+from clickstream 
+where prev regexp "Hotel_California" and types = "link"  
+group by prev 
+order by visit desc
+limit 10;
+
+-- Create an external table to store all the data from the fike
+create external table data (domain_code string, page_title string, count_views int, total_response_size int)
+row format delimited
+fields terminated by ' '
+stored as textfile
+;
+
+-- Loading the data from file to the table
+load DATA INPATH "/user/maria_dev/data/pageviews-20210101-000000" into table data;
+
+-- Creating a table to store data for only German
+create table de_data as select * from us_data where domain_code regexp "de";
+
+-- Creating a table to store data for only English
+create table en_data as select * from us_data where domain_code regexp "en";
+
+-- Question 4
+select en.page_title, sum(en.count_views) as us_visits, de.page_title ,sum(de.count_views) as ge_visits from en_data en
+join de_data de
+where en.page_title = de.page_title 
+group by en.page_title, de.page_title
+having us_visits > ge_visits 
+order by us_visits desc, ge_visits
 limit 20;
 
-#5
-SELECT lang, SUM(views) from pageviews where lang='en.m' OR lang='en' GROUP BY lang;
+-- Create a table to upload the data from the files
+CREATE EXTERNAL TABLE IF NOT EXISTS pageviews1(lang STRING,	
+					   page STRING,
+					   views INT)
+					   ROW FORMAT DELIMITED
+					   FIELDS TERMINATED BY ' '
+					   STORED AS TEXTFILE;
+					   
+-- Loading the files into the table
+LOAD DATA INPATH '/user/maria_dev/data/formated_00.txt' INTO TABLE pageviews1;
+LOAD DATA INPATH '/user/maria_dev/data/formated_01.txt' INTO TABLE pageviews1;
+LOAD DATA INPATH '/user/maria_dev/data/formated_02.txt' INTO TABLE pageviews1;
+.
+.
+.
+LOAD DATA INPATH '/user/maria_dev/data/formated_23.txt' INTO TABLE pageviews1;
+
+
+-- Question 5
+select domain_code, sum(count_views) from data_01 
+group by domain_code
+having domain_code regexp "en.m$|en$";
